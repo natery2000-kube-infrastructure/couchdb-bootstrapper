4d2a9ee2fb2acfbacdd8b152d58e7b24dcce3a00
Nathan <you@example.com>
2022-07-29 20:15:51 -0500

    Reading mounted config file

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("hello world")
+	data, err := os.ReadFile("/config/schema.json")
+	if err != nil {
+		fmt.Println("error reading /config/schema.json")
+	}
+
+	schema := schema{}
+	json.Unmarshal([]byte(data), &schema)
+
+	fmt.Println(schema)
+}
+
+type schema struct {
+	Databases []string `json:"databases"`
 }
